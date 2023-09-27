de763c57ab414cfe6e791bb24f39f6682de27a78
ivanka08 <ivankahudic@gmail.com>
2023-09-28 00:00:04 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMurkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+function createMurkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join("")
+}
+  
 
 console.log(galleryItems);
