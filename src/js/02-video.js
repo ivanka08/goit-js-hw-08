fc70e53b3cb9017a75f6fe2c277fa4d44e5b61e9
ivanka08 <ivankahudic@gmail.com>
2023-09-28 19:00:49 +0300

    Tasks 1,2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function saveCurrentTimeToLocalStorage(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+function restorePlaybackFromLocalStorage() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime !== null) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+}
+
+const onPlay = throttle((event) => {
+  saveCurrentTimeToLocalStorage(event.seconds);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+player.ready().then(() => {
+  restorePlaybackFromLocalStorage();
+});
