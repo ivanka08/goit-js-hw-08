18bbbcd9b79a0cf341c7835a7e0681a546e1624c
ivanka08 <ivankahudic@gmail.com>
2023-09-28 19:36:34 +0300

    Tasks 1,2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+  
+const saveFormState = throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }, 500);
+
+const loadFormState = () => {
+    const savedFormState = localStorage.getItem('feedback-form-state');
+
+    if (savedFormState) {
+      const formState = JSON.parse(savedFormState);
+      emailInput.value = formState.email;
+      messageInput.value = formState.message;
+    }
+};
+  
+  window.addEventListener('load', loadFormState);
+
+  form.addEventListener('input', saveFormState);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+      
+    localStorage.removeItem('feedback-form-state');
+
+    emailInput.value = '';
+    messageInput.value = '';
+
+    console.log(formState);
+  });
